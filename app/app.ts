5c72a02ffdec8d1dfa4286b24339c8345dc374a8
dario russo <dario.russo.052001@gmail.com>
2023-08-23 16:55:32 +0100

    security: use helmet library & disables x-powered-by

--- app/app.ts
+++ app/app.ts
@@ -4,10 +4,16 @@ dotenv.config();
 
 import express, { Application } from "express";
 import routes from "./routes/routes";
-import errorHandlerMiddleware from "./api/middlewares/ErrorHandlerMiddleware";
+import errorHandlerMiddleware from "./api/middlewares/errorHandlerMiddleware";
+import helmet from "helmet";
 
 // start express app
 const app: Application = express();
+
+// use the helmet library for improving security as per https://expressjs.com/en/advanced/best-practice-security.html#use-helmet
+app.use(helmet());
+// disables x-powered-by for improved security against casual exploiters as per https://expressjs.com/en/advanced/best-practice-security.html#use-helmet
+app.disable("x-powered-by");
 
 app.use(express.json());
 routes(app);
